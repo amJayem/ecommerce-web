e43aae75df880b5dc07776fd657836483eff1edb
amjayem <csejayem19@gmail.com>
2025-07-07 00:20:04 +0600

    added: metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ import { HeroSection } from '@/components/hero-section'
 import { Navbar } from '@/components/navbar'
 import ProductGrid from '@/components/product-grid'
 
+export const metadata = {
+  title: 'Buy Organic Products Online - GroceryFresh',
+  description: 'Shop fresh, organic groceries delivered to your door.',
+};
+
+
 export default function HomePage() {
   return (
     <main className='p-4'>
